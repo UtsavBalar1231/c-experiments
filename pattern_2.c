ba2050c925e30d5c98a4a230b6955ea0400dcf72
utsavbalar1231 <utsavbalar1231@gmail.com>
2020-01-31 21:21:57 +0530

    c-experiments: add a program for pattern 2
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

---
+++ pattern_2.c
@@ -0,0 +1,25 @@
+/*
+	AAAAA
+	BBBB
+	CCC
+	DD
+	E
+*/
+
+#include <stdio.h>
+
+void main()
+{
+    int i, j;
+
+     for(i = 1; i <= 5; i++)
+     {
+          for(j = 5; j >= i; j--)
+          {
+                   printf("%c", 'A'-1 + i);
+          }
+
+     printf("\n");
+    }
+
+}
