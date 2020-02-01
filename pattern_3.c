61f881e2564bffb66061397e43c77c0c19ff1cb7
utsavbalar1231 <utsavbalar1231@gmail.com>
2020-02-01 19:59:49 +0530

    c-experiments: pattern3
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

---
+++ pattern_3.c
@@ -0,0 +1,33 @@
+/*
+	*****
+	 ****
+	  ***
+	   **
+	    *
+ */
+
+#include <stdio.h>
+
+int main()
+{
+  int i, j, k, n;
+  printf("Enter the no of rows: \n");
+  scanf("%d", &n);
+
+  for(i = 1; i <= n; i++)
+  {
+	for(k = 1; k < i; k++)
+	{
+		printf(" ");
+	}
+
+	for(j = i; j <= n; j++)
+	{
+		printf("*");
+	}
+
+  printf("\n");
+  }
+
+  return 0;
+}
